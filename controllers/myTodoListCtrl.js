ae077d5006076363b340142f55565d253ce6fc9a
jingyuh@andrew.cmu.edu <jingyuh@andrew.cmu.edu>
2015-10-06 00:20:34 -0700

    final

--- controllers/myTodoListCtrl.js
+++ controllers/myTodoListCtrl.js
@@ -1,112 +1,154 @@
 /**
  * Created by shanshan on 9/30/15.
  */
-app.controller('myTodoListCtrl', function($scope, $location, taskFactory, $http){
-    $scope.isViewing=false;
-    $scope.isDisabled=false;
-    $scope.format='M/d/yy';
-    $scope.allTasks=taskFactory.getTasks();
-    $scope.task={taskId:0, taskName: "", taskStatus: 'active'};
-    $scope.task.taskId=taskFactory.getLength();
-    $scope.showTasks=$scope.allTasks;
-    $scope.sequence=0;
+app.controller('myTodoListCtrl', function ($scope, $location, taskFactory, $http) {
 
-    //$http.defaults.headers.get={'Content-Type':'text/plain'};
+    $scope.task = {taskId: 0, taskName: "", taskStatus: 'active'};
+    $scope.isViewing = false;
+    $scope.isDisabled = false;
+    $scope.format = 'M/d/yy';
+    $scope.sequence = 0;
 
+    //for initialization
+    var server = 'http://www.codeee.com:8080/task/';
+    $scope.init = function () {
+        $http.get(server + 'allTasks'
+        ).success(function (data) {
+                //because angular $http is asynchronous
+                //so this thread will run in background
+                // so $http.get is another thread
+                // to make sure populating successful
+                // we should put these stuff here
+                $scope.allTasks = data;
+                taskFactory.setTasks($scope.allTasks);
+                $scope.task.taskId = taskFactory.getLength();
+                $scope.showTasks = $scope.allTasks;
+            })
+    }
+    $scope.init();
 
-    $scope.addTask = function(){
+    $scope.addTask = function () {
+        var task = $scope.task;
         //var task = $scope.task;
         //task.taskStatus=$scope.task.taskStatus;
         taskFactory.addTask($scope.task);
-        $scope.task={taskId:0, taskName: "", taskStatus: 'active'};
-        $scope.task.taskId=taskFactory.getLength();
-        switch($scope.sequence) {
-        case 0:
-            break;
+        $scope.task = {taskId: 0, taskName: "", taskStatus: 'active'};
+        $scope.task.taskId = taskFactory.getLength();
+        switch ($scope.sequence) {
+            case 0:
+                break;
 
-        case 1:
-            var activeTasks = $scope.allTasks.filter(function(element){
-                return element.taskStatus=='active';
-            });
-            $scope.showTasks=activeTasks;
+            case 1:
+                var activeTasks = $scope.allTasks.filter(function (element) {
+                    return element.taskStatus == 'active';
+                });
+                $scope.showTasks = activeTasks;
 
-            break;
+                break;
 
-        case 2:
-            var completedTasks = $scope.allTasks.filter(function(element){
-                return element.taskStatus=='completed';
-            });
-            $scope.showTasks=completedTasks;
+            case 2:
+                var completedTasks = $scope.allTasks.filter(function (element) {
+                    return element.taskStatus == 'completed';
+                });
+                $scope.showTasks = completedTasks;
 
         }
 
-        //interact with backend
-        $http.post('http://www.codeee.com:8080/task/insert',
-            {taskId:$scope.task.taskId,taskName:$scope.task.taskName,taskStatus:$scope.task.taskStatus})
-            .success(function(data){
 
+        //interact with backend
+
+        $http.post(server + 'insert', task
+        ).success(function (data) {
+                if (data == true)
+                    alert("success");
+                else
+                    alert("fail, maybe task id already exists");
             })
             .error();
 
 
     }
 
-    $scope.clearTask=function(){
-        $scope.isViewing=false;
-        $scope.task.taskName="";
-        $scope.task.taskStatus="active";
+    $scope.clearTask = function () {
+        $scope.isViewing = false;
+        $scope.task.taskName = "";
+        $scope.task.taskStatus = "active";
 
     }
 
-    $scope.updateTask=function(){
-        $scope.isViewing=false;
-        $scope.task='';
-        $scope.isDisabled=false;
+    $scope.updateTask = function () {
+        var task = $scope.task;
+        $scope.isViewing = false;
+        $scope.task = '';
+        $scope.isDisabled = false;
+
+        //interact with backend
+        $http.post(server + 'update', task
+        ).success(function (data) {
+                if (data == true)
+                    alert("success");
+                else
+                    alert("fail, maybe task does not exist");
+            })
+            .error();
+
     }
 
-    $scope.editTask=function(id){
-          $scope.isDisabled=true;
-          $scope.isViewing=true;
-          var task = taskFactory.getTaskById(id);
-          $scope.task=task;
+    $scope.editTask = function (id) {
+        $scope.isDisabled = true;
+        $scope.isViewing = true;
+        var task = taskFactory.getTaskById(id);
+        $scope.task = task;
 
         //$scope.task.taskName = task.taskName;
         //$scope.task.taskStatus =task.taskStatus;
 
     }
 
-    $scope.deleteTask=function(id){
+    $scope.deleteTask = function (id) {
 
-        var task =taskFactory.getTaskById(id);
-        task.taskStatus="deleted";
+        var task = taskFactory.getTaskById(id);
+        task.taskStatus = "deleted";
+
+        //interact with backend
+        $http.post(server + 'delete/' + id, task
+        ).success(function (data) {
+                if (data == true)
+                    alert("delete success");
+                else
+                    alert("fail, maybe task does not exist");
+            })
+            .error();
+
+
     }
 
-    $scope.showAll=function(){
-        $scope.showTasks= $scope.allTasks;
-        $scope.sequence=0;
+    $scope.showAll = function () {
+        $scope.showTasks = $scope.allTasks;
+        $scope.sequence = 0;
 
     }
 
-    $scope.showActive=function(){
-         var activeTasks = $scope.allTasks.filter(function(element){
-            return element.taskStatus=='active';
+    $scope.showActive = function () {
+        var activeTasks = $scope.allTasks.filter(function (element) {
+            return element.taskStatus == 'active';
         });
-        $scope.showTasks=activeTasks;
-        $scope.sequence=1;
+        $scope.showTasks = activeTasks;
+        $scope.sequence = 1;
     }
 
-    $scope.showCompleted=function(){
+    $scope.showCompleted = function () {
 
-        var completedTasks = $scope.allTasks.filter(function(element){
-            return element.taskStatus=='completed';
+        var completedTasks = $scope.allTasks.filter(function (element) {
+            return element.taskStatus == 'completed';
         });
 
-        $scope.showTasks=completedTasks;
-        $scope.sequence=2;
+        $scope.showTasks = completedTasks;
+        $scope.sequence = 2;
 
     }
 
-    $scope.switchOrder=function(){
+    $scope.switchOrder = function () {
         var fromId = $scope.fromId;
         var toId = $scope.toId;
 
@@ -121,18 +163,15 @@ app.controller('myTodoListCtrl', function($scope, $location, taskFactory, $http)
         toTask.taskName = temp;
 
         temp = toTask.taskStatus;
-        toTask.taskStatus=fromTask.taskStatus;
-        fromTask.taskStatus=temp;
+        toTask.taskStatus = fromTask.taskStatus;
+        fromTask.taskStatus = temp;
 
         //toTask = {taskId: toId, taskName: fromTask.taskName, taskStatus: fromTask.taskStatus };
         //fromTask = { taskId: fromId, taskName: toTask.taskName, taskStatus: toTask.taskStatus};
 
-       //$scope.allTasks = taskFactory.getTasks();
+        //$scope.allTasks = taskFactory.getTasks();
 
     }
-
-
-
 
 
 });
--- factory/taskFactory.js
+++ factory/taskFactory.js
@@ -3,8 +3,9 @@
  */
 app.factory('taskFactory', function(){
     var obj = {};
-    var data=[{taskId:1, taskName: "take a shower", taskStatus: 'active'},{taskId:2, taskName: "take a bath", taskStatus: 'completed'}];
+    //var data=[{taskId:1, taskName: "take a shower", taskStatus: 'active'},{taskId:2, taskName: "take a bath", taskStatus: 'completed'}];
 
+    var data=[];
     obj.addTask = function(task){
         //task.taskId=data.length+1;
         data.push(task);
@@ -15,6 +16,10 @@ app.factory('taskFactory', function(){
         return data;
     }
 
+    //loading data from server
+    obj.setTasks=function(data1){
+        data=data1;
+    }
 
     obj.getTaskById=function(id){
         var task =null;
